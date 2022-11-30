6e52128f93ac59cd7e5e38df4b4fd494e5ea671c
Alexandra Larsen <Larsen.Alexandra@epa.gov>
2022-11-30 12:22:43 -0500

    Add Kwon SA
    Compare overall results when using estimates on different log bases.

--- data_preprocess_pfna.R
+++ data_preprocess_pfna.R
@@ -1,3 +1,5 @@
+# PFNA data pre-processing
+
 library(readxl)
 library(tidyr)
 library(tidyverse)
@@ -11,6 +13,8 @@ library(RColorBrewer)
 
 setwd("C:/Users/alarsen/OneDrive - Environmental Protection Agency (EPA)/Projects/IRIS/PFAS/pfas-birthweight-meta-analysis")
 source("utility_functions.R")
+
+# Primary data set -------------------------------------------------------------
 
 betas <- read_excel("PFNA_Unit_Conversion_v6.xlsx") %>%
   as.data.frame() %>%
@@ -42,7 +46,6 @@ sampling2 <- sampling %>%
   pivot_longer(Mean:Exact, names_to = "Summary_Type") %>%
   rename(GA = value) %>%
   drop_na()
-
 sampling <- left_join(sampling, sampling2, by = "Study")
 
 betas <- betas  %>%     ## PFNA
@@ -52,7 +55,41 @@ betas <- betas  %>%     ## PFNA
          Sampling_1 = factor(Sampling_1), 
          Sampling_2 = factor(Sampling_2), 
          Sampling_3 = factor(Sampling_3))
-
-#View(betas)
 write.csv(betas, "betas_pfna.csv", row.names = F)
 
+# Data set for Sensitivity Analysis on Kwon, 2016 ------------------------------
+
+betas_kwon <- read_excel("PFNA_Unit_Conversion_v2_kwon_options.xlsx") %>%
+  as.data.frame() %>%
+  select(c("Study2", "Sample Size", "Population", "Confidence",
+           "BetaOrig", "BetaOrigLCL", "BetaOrigUCL", "BetaUnit",
+           "BetaNgml", "BetaNgmlSE", "BetaNgmlLCL", "BetaNgmlUCL",
+           "BetaLnNgml", "BetaLnNgmlSE", "BetaLnNgmlLCL", "BetaLnNgmlUCL")) %>%
+  rename(Study = Study2, Sample.Size = `Sample Size`) %>% 
+  separate(col = Study, into = c("Author", "Year"), sep = ",", remove = F) %>% 
+  filter(Author == "Kwon" & BetaUnit != "g/ln(ng/ml)") %>%
+  mutate(Confidence = factor(Confidence, levels = c("High", "Medium", "Low")),
+         Population = factor(Population)) %>%
+  mutate_at(c("BetaNgml", "BetaNgmlSE", "BetaNgmlLCL", "BetaNgmlUCL"), as.double)
+
+sampling <- read_excel("Sample_Timing.xlsx", sheet = "Sample_Timing") %>%
+  unite("Study", Author:Year, sep = ", ")
+
+# TODO re-code to take medians over means, etc. if more than one is reported
+sampling2 <- sampling %>%
+  select(Study, Mean:Exact) %>%
+  pivot_longer(Mean:Exact, names_to = "Summary_Type") %>%
+  rename(GA = value) %>%
+  drop_na()
+sampling <- left_join(sampling, sampling2, by = "Study")
+
+betas_kwon <- betas_kwon %>%
+  select(!c(Author, Year)) %>%
+  left_join(sampling[, c("Study", "GA", "Bin", "Sampling_1", "Sampling_2", "Sampling_3")], by = "Study") %>%
+  mutate(Bin = factor(Bin), 
+         Sampling_1 = factor(Sampling_1), 
+         Sampling_2 = factor(Sampling_2), 
+         Sampling_3 = factor(Sampling_3)) %>%
+  bind_rows(read.csv("betas_pfna.csv"))
+
+write.csv(betas_kwon, "betas_pfna_kwon_options.csv")
